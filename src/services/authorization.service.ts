ea6ecba132ce3e3b92b3e42ce0abfe86ea49e54e
Sorokovsky <Sorokovskys@ukr.net>
2025-11-11 19:15:15 +0200

    authorization service

--- src/services/authorization.service.ts
+++ src/services/authorization.service.ts
@@ -2,7 +2,7 @@ import {inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {LOGIN_URL, LOGOUT_URL, REGISTRATION_URL} from '@/constants/backend-api.constants';
 import {type User, UserSchema} from '@/schemes/user.schema';
-import {map, type Observable} from 'rxjs';
+import {lastValueFrom, map, type Observable} from 'rxjs';
 import {type RegisterUser} from '@/schemes/register-user.schema';
 import {type LoginUser} from '@/schemes/login-user.schema';
 
@@ -12,19 +12,22 @@ import {type LoginUser} from '@/schemes/login-user.schema';
 export class AuthorizationService {
   private readonly httpClient: HttpClient = inject(HttpClient);
 
-  public register(registerDto: RegisterUser): Observable<User> {
-    return this.httpClient.post(REGISTRATION_URL, registerDto)
+  public async register(registerDto: RegisterUser): Promise<User> {
+    const response: Observable<User> = this.httpClient.post(REGISTRATION_URL, registerDto)
       .pipe(map((response: unknown): User => UserSchema.parse(response)));
+    return await lastValueFrom(response);
   }
 
-  public login(loginDto: LoginUser): Observable<User> {
-    return this.httpClient.put(LOGIN_URL, loginDto)
+  public async login(loginDto: LoginUser): Promise<User> {
+    const response: Observable<User> = this.httpClient.put(LOGIN_URL, loginDto)
       .pipe(map((response: unknown): User => UserSchema.parse(response)));
+    return await lastValueFrom(response);
   }
 
-  public logout(): Observable<void> {
-    return this.httpClient.delete(LOGOUT_URL)
+  public async logout(): Promise<void> {
+    const response: Observable<void> = this.httpClient.delete(LOGOUT_URL)
       .pipe(map((): void => {
-      }))
+      }));
+    return await lastValueFrom(response)
   }
 }
