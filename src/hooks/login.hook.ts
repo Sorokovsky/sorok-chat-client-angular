8eaabbfc51cc81d68191489db4fa4ecd62821261
Sorokovsky <Sorokovskys@ukr.net>
2025-11-11 21:43:37 +0200

    refactor

--- src/hooks/login.hook.ts
+++ src/hooks/login.hook.ts
@@ -1,12 +1,16 @@
 import {AuthorizationService} from '@/services/authorization.service';
-import {type CreateMutationOptions, injectMutation} from '@tanstack/angular-query-experimental';
+import {
+  type CreateMutationOptions,
+  type CreateMutationResult,
+  injectMutation
+} from '@tanstack/angular-query-experimental';
 import {inject} from '@angular/core';
 import type {User} from '@/schemes/user.schema';
 import {lastValueFrom} from 'rxjs';
 import {LOGIN_KEY} from '@/constants/queries.constants';
 import {LoginUser} from '@/schemes/login-user.schema';
 
-export const useLogin = () => {
+export function useLogin(): CreateMutationResult<User, Error, LoginUser, void> {
   const authorizationService: AuthorizationService = inject(AuthorizationService);
   return injectMutation<User, Error, LoginUser, void>((): CreateMutationOptions<User, Error, LoginUser, void> => ({
       mutationKey: [LOGIN_KEY],
--- src/hooks/profile.hook.ts
+++ src/hooks/profile.hook.ts
@@ -1,13 +1,13 @@
 import {AuthorizationService} from '@/services/authorization.service';
 import {inject} from '@angular/core';
-import {injectQuery} from '@tanstack/angular-query-experimental';
+import {type CreateQueryOptions, type CreateQueryResult, injectQuery} from '@tanstack/angular-query-experimental';
 import {PROFILE_KEY} from '@/constants/queries.constants';
 import {lastValueFrom} from 'rxjs';
 import {type User} from '@/schemes/user.schema';
 
-export const useProfile = () => {
+export function useProfile(): CreateQueryResult<User> {
   const authorizationService: AuthorizationService = inject(AuthorizationService);
-  return injectQuery(() => ({
+  return injectQuery((): CreateQueryOptions<User> => ({
     queryKey: [PROFILE_KEY],
     queryFn: async (): Promise<User> => {
       return await lastValueFrom(authorizationService.getProfile());
