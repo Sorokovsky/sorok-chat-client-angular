0f07c00c088fddd99ade57f37d9a1fe672195a4e
Sorokovsky <Sorokovskys@ukr.net>
2025-11-11 20:37:54 +0200

    refactor

--- src/components/layouts/main-layout/main-layout.ts
+++ src/components/layouts/main-layout/main-layout.ts
@@ -1,7 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {ChatsSidebar} from '@/components/common/chats-sidebar/chats-sidebar';
 import {Header} from '@/components/common/header/header';
-import {useProfile} from '@/hooks/profile.hook';
 
 @Component({
   selector: 'app-main-layout',
@@ -12,10 +11,5 @@ import {useProfile} from '@/hooks/profile.hook';
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
-export class MainLayout implements OnInit {
-  profile = useProfile();
-
-  ngOnInit(): void {
-    console.log(this.profile);
-  }
+export class MainLayout {
 }
