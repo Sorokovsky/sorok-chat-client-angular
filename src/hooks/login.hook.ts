1625c2e562f6c82292902b3b92b05fb76a63629e
Sorokovsky <Sorokovskys@ukr.net>
2025-11-12 21:59:51 +0200

    refetch on login

--- src/hooks/login.hook.ts
+++ src/hooks/login.hook.ts
@@ -2,21 +2,25 @@ import {AuthorizationService} from '@/services/authorization.service';
 import {
   type CreateMutationOptions,
   type CreateMutationResult,
-  injectMutation
+  injectMutation,
+  QueryClient
 } from '@tanstack/angular-query-experimental';
 import {inject} from '@angular/core';
 import type {User} from '@/schemes/user.schema';
 import {lastValueFrom} from 'rxjs';
-import {LOGIN_KEY} from '@/constants/queries.constants';
+import {LOGIN_KEY, PROFILE_KEY} from '@/constants/queries.constants';
 import {LoginUser} from '@/schemes/login-user.schema';
 
 export function useLogin(): CreateMutationResult<User, Error, LoginUser, void> {
   const authorizationService: AuthorizationService = inject(AuthorizationService);
+  const queryClient: QueryClient = inject(QueryClient);
   return injectMutation<User, Error, LoginUser, void>((): CreateMutationOptions<User, Error, LoginUser, void> => ({
-      mutationKey: [LOGIN_KEY],
-      mutationFn: async (data: LoginUser): Promise<User> => {
-        return await lastValueFrom<User>(authorizationService.login(data))
-      },
-    })
-  )
+    mutationKey: [LOGIN_KEY],
+    mutationFn: async (data: LoginUser): Promise<User> => {
+      return await lastValueFrom<User>(authorizationService.login(data))
+    },
+    onSettled: async () => {
+      await queryClient.refetchQueries({queryKey: [PROFILE_KEY], exact: false});
+    },
+  }))
 }
